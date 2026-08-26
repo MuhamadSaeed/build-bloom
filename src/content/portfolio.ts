9a34854af08511e32789e8ceb67f8a303b3ef5b5
MuhamadSaeed <mohamedsaiedz2156287@gmail.com>
2026-08-26 18:39:24 +0300

    Update portfolio projects

--- src/content/portfolio.ts
+++ src/content/portfolio.ts
@@ -1,38 +1,28 @@
 export const PORTFOLIO_CONTENT = [
   {
-    name: "Landscaping Website",
+    name: "E-commerce Website",
     description:
-      "A modern website concept for a landscaping business, featuring service showcases, project galleries, and quote requests.",
-    img: "/images/portfolio/eco.jpg",
+      "A complete e-commerce website with product browsing, categories, secure user authentication, shopping cart, admin product management, and online payments.",
+    img: "/images/portfolio/ecommerce.jpeg",
   },
+
   {
-    name: "Event Promotion Website",
+    name: "POS & Financial Management System",
     description:
-      "A vibrant promotional website concept for events, featuring ticket booking, photo galleries, and engaging event content.",
-    img: "/images/portfolio/event.jpg",
+      "A powerful business management system with a high-speed POS, inventory management, customer CRM, supplier management, invoices, financial accounts, and real-time business analytics.",
+    img: "/images/portfolio/pos.jpeg",
   },
+
   {
-    name: "College Website",
+    name: "Nutrivex, AI Wellness App",
     description:
-      "An elegant educational website concept featuring academic programs, campus information, galleries, and application-focused sections.",
-    img: "/images/portfolio/art.jpg",
+      "An AI-powered nutrition and fitness app that helps users track their nutrition, discover exercises, calculate BMI, and receive personalized wellness guidance.",
+    img: "/images/portfolio/nutrivex.jpeg",
   },
-  // {
-  //   name: "Bike Rental Platform Concept",
-  //   description:
-  //     "A modern bike rental platform concept with availability, pricing, rental options, and a streamlined booking experience.",
-  //   img: "/images/portfolio/bike.jpg",
-  // },
-  {
-    name: "Restaurant Website",
+    {
+    name: "Educational Platform",
     description:
-      "A modern restaurant website concept featuring menu presentation, reservations, special offers, and a polished dining experience.",
-    img: "/images/portfolio/resturant.jpg",
-  },
-  {
-    name: "Meditation App",
-    description:
-      "A mobile app concept focused on guided meditation, session timers, progress tracking, and a calm user experience.",
-    img: "/images/portfolio/meditation.jpg",
+      "A modern educational platform designed to deliver engaging learning experiences with course content, student management, interactive learning features, and a seamless user experience.",
+    img: "/images/portfolio/education.png",
   },
 ];
